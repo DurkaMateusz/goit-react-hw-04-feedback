04b8960ec2c827d707ee61b349be6c7807e6a098
DurkaMateusz <mosca888@gmail.com>
2024-02-27 11:54:48 +0100

    Update App.jsx
    working countertotal and percentage of positive feedbacks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,11 +8,21 @@ export class App extends Component {
       good: 0,
       neutral: 0,
       bad:0,
+      total: 0,
+      positiveFeedback: 0,
     }
   }
 
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;    
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return total ? Math.round((this.state.good / total) * 100) : 0;
+  };
+
   render() {
-    
     return (
       <div className="feedback">
         <section>
@@ -33,6 +43,8 @@ export class App extends Component {
                 <p>Good: {this.state.good}</p>
                 <p>Neutral: {this.state.neutral}</p>
                 <p>Bad: {this.state.bad}</p>
+                <p>Total: {this.countTotalFeedback()}</p>
+                <p>Positive Feedback: {this.countPositiveFeedbackPercentage()}</p>
             </div>
         </section>
       </div>
