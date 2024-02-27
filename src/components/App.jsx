493f4ab98dc3f01735c2d2d7652e491759e6b4f7
DurkaMateusz <mosca888@gmail.com>
2024-02-27 11:37:10 +0100

    feedback step 1 done
    working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+export class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad:0,
+    }
+  }
+
+  render() {
+    
+    return (
+      <div className="feedback">
+        <section>
+        <h2>please leave feedback</h2>
+        <div>
+          <button onClick={() => this.setState(good => ({good: this.state.good +1}))}>
+              good
+            </button>
+            <button onClick={() => this.setState(neutral => ({neutral: this.state.neutral +1}))}>
+              neutral
+            </button>
+            <button onClick={() => this.setState(bad => ({bad: this.state.bad +1}))}>
+              bad
+            </button>
+        </div>
+            <h2>statistic</h2>
+            <div>
+                <p>Good: {this.state.good}</p>
+                <p>Neutral: {this.state.neutral}</p>
+                <p>Bad: {this.state.bad}</p>
+            </div>
+        </section>
+      </div>
+    );
+  }
+}
